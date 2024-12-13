186bb71d111c9850c22bd1979cfa54e53c307c6b
DangerDdos <92170883+DangerDdos@users.noreply.github.com>
2024-12-14 01:30:55 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let age = prompt("Masukkan umur orang yang berulang tahun:");
+    document.getElementById('age').innerText = age;
+    alert("Selamat Ulang Tahun!");
+});
