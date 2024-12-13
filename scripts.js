d984d45c6232c8823da20442d7e9079fc476f92e
DangerDdos <92170883+DangerDdos@users.noreply.github.com>
2024-12-14 01:36:58 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let age = prompt("Masukkan umur orang yang berulang tahun:");
-    document.getElementById('age').innerText = age;
-    alert("Selamat Ulang Tahun!");
-});
+
